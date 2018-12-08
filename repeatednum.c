bac289efda2ca1830b603e8af0e08ed708db86be
PradipBanik <45711093+PradipBanik@users.noreply.github.com>
2018-12-08 17:44:09 +0530

    Update repeatednum.c

--- repeatednum.c
+++ repeatednum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int a[100];
+int a[100],b[100],c[100];
 int size,i,j,p=0;
 scanf("%d",&size);
 for(i=0;i<size;i++)
@@ -20,7 +20,11 @@ for(i=0;i<size;i++)
  {
   if(a[i]==a[j])
   {
-   printf("%d",a[i]);
+      if(j<i)
+      {
+          break;
+      }
+   b[p]=a[i];
    p++;
    break;
   }
@@ -31,5 +35,24 @@ if(p==0)
 {
 printf("unique");
 }
+else{
+    for(i=0;i<p;i++)
+    {
+        int count=0;
+        for(j=0;j<p;j++)
+        {
+            if(b[i]>b[j])
+            {
+                count++;
+            }
+        }
+        c[count]=b[i];
+    }
+    for(i=0;i<p;i++)
+    {
+        printf("%d",c[i]);
+    }
+}
 return 0;
 }
+
